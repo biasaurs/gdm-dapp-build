fb755b14b90cfb8e49acda2946d4752fd39118bb
biasaurs <biasaurs@gmail.com>
2022-07-27 11:41:46 +0500

    first commit

---
+++ precache-manifest.e0ce37f20ea4745edb131bb29a456ca4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9450c0ada0006290d05db9aca6d30a5d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "734a9afef50ff51d51ff",
+    "url": "/static/css/main.de453e59.chunk.css"
+  },
+  {
+    "revision": "5a6b6b87c4f96389b39a",
+    "url": "/static/js/2.c5aed569.chunk.js"
+  },
+  {
+    "revision": "7ccf4f45ad352995adb5",
+    "url": "/static/js/3.ba25b633.chunk.js"
+  },
+  {
+    "revision": "734a9afef50ff51d51ff",
+    "url": "/static/js/main.ff57ea0a.chunk.js"
+  },
+  {
+    "revision": "52ca75699542e4e27d1b",
+    "url": "/static/js/runtime~main.df8a789f.js"
+  },
+  {
+    "revision": "e63562614cbdc491e4d374f97c513b82",
+    "url": "/static/media/font.e6356261.woff"
+  }
+]);
